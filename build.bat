936734303da9d2081b64d3e389196e35b1e6dbb5
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2023-02-13 04:38:44 +0900

    warning

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @rem cl /openmp %*
 @rem cl /openmp:experimental %*
 @rem cl /openmp /O2 %*
-cl /openmp:experimental /O2 %*
+cl /openmp:experimental /O2 /W4 %*
 @rem cl /openmp:experimental /Ox %*
 @rem cl /O2 %*
---
+++ build.sh
@@ -0,0 +1,1 @@
+ cc -O2 -fopenmp -Wall $*
