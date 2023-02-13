60346c214cc627807735182b98f8bb7669528893
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2023-02-13 17:05:50 +0900

    critical

---
+++ critical_openmp.c
@@ -0,0 +1,36 @@
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define DATA_SIZE 100000
+int main()
+{
+    /* code */
+    double Max=-INFINITY;
+    double Min=INFINITY;
+    int LoopNum = DATA_SIZE;
+    double Data[DATA_SIZE];
+    for(int i=0;i<LoopNum; i++) {
+        Data[i] = (double)i;
+    }
+    #pragma omp parallel
+    {
+        double TempMax=-INFINITY,TempMin=INFINITY;
+        int i;
+        #pragma omp for
+        for(i=0;i<LoopNum;i++) {
+            if(TempMax<Data[i]) TempMax=Data[i];
+            if(TempMin>Data[i]) TempMin=Data[i];
+        }
+        #pragma omp critical
+        {
+            if(Max<TempMax) Max=TempMax;
+            if(Min>TempMin) Min=TempMin;
+        }
+    }
+    printf("min=%lf, max=%lf\n",Min,Max);
+    return 0;
+}
